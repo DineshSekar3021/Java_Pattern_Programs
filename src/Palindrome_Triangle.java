032804c23bb819bb0c1f5fb3837e20486e3c5eba
Dinesh <dinesh12032002@gmail.com>
2024-07-20 23:19:34 +0530

    Commit

--- src/Palindrome_Triangle.java
+++ src/Palindrome_Triangle.java
@@ -1,4 +1,4 @@
-public class Palndrome_Traingle {
+public class Palindrome_Triangle {
     public static void main(String[] args) {
         for (int i=1;i<5;i++){
             for(int j=5;j>i;j--){
