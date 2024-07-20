9bd237cf5bed74c89eb0a8c85db37f9b6421df15
Dinesh <dinesh12032002@gmail.com>
2024-07-20 22:57:16 +0530

    Commit

--- src/Palndrome_Traingle.java
+++ src/Palndrome_Traingle.java
@@ -1,2 +1,16 @@
 public class Palndrome_Traingle {
+    public static void main(String[] args) {
+        for (int i=1;i<5;i++){
+            for(int j=5;j>i;j--){
+                System.out.print("  ");
+            }
+            for (int k=i;k>0;k--){
+                System.out.print(k+" ");
+            }
+            for(int l=i;l>1;l--){
+                System.out.print(l+" ");
+            }
+            System.out.println();
+        }
+    }
 }
