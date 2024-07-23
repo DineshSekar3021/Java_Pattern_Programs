e4c6a558a321590ba32c111a3f0e6eb812d57243
Dinesh <dinesh12032002@gmail.com>
2024-07-23 13:08:14 +0530

    Commit

--- src/Butterfly_Star_Pattern.java
+++ src/Butterfly_Star_Pattern.java
@@ -1,26 +1,25 @@
 public class Butterfly_Star_Pattern {
     public static void main(String[] args) {
-        int a=8;
+        int a=10;
         for (int i=1;i<5;i++){
             for(int j=1;j<=i;j++){
-//                if(((i+j)%2)==0){
+                if(((i+j)%2)==0){
                     System.out.print("*"+" ");
-//                }else {
-//                    System.out.print("  ");
-//                }
+                }else {
+                    System.out.print("  ");
+                }
             }
-
             for(int k=a;k>0;k--){
-                System.out.print(" ");
+                System.out.print("  ");
             }
             for (int l=1;l<=i;l++){
-//                if(((i+l)%2)==0){
+                if((l%2)!=0){
                     System.out.print("*"+" ");
-//                }else {
-//                    System.out.print("  ");
-//                }
+                }else {
+                    System.out.print("  ");
+                }
             }
-            a--;
+a=a-2;
             System.out.println();
         }
     }
