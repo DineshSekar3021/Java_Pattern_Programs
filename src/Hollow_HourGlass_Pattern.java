69eebce1e3eec5f5f7bac2cb39dd8abe7c9d14fd
Dinesh <dinesh12032002@gmail.com>
2024-07-23 18:21:13 +0530

    Commit

---
+++ src/Hollow_HourGlass_Pattern.java
@@ -0,0 +1,34 @@
+public class Hollow_HourGlass_Pattern {
+    public static void main(String[] args) {
+        int a=1;
+        for (int i=4;i>0;i--){
+            for(int j=a;j>0;j--){
+                System.out.print(" ");
+            }
+            for (int k=1;k<=i;k++){
+                if(k==1 ||i==4 ||k==i){
+                System.out.print("*"+" ");
+            }else{
+                System.out.print("  ");
+            }
+
+        }
+            a++;
+            System.out.println();
+    }
+        int b=3;
+        for (int i=2;i<5;i++){
+            for (int j=b;j>0;j--){
+                System.out.print(" ");
+            }
+            for (int k=1;k<=i;k++){
+                if (k==1 || k==i||i==4){
+                    System.out.print("*"+" ");
+                }else {
+                    System.out.print("  ");
+                }
+            }
+            b--;
+            System.out.println();
+        }
+}}
