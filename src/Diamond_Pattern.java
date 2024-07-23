bec1ef054ca028671fea01333c87612e8dab1f81
Dinesh <dinesh12032002@gmail.com>
2024-07-23 10:13:14 +0530

    Commit

---
+++ src/Diamond_Pattern.java
@@ -0,0 +1,25 @@
+public class Diamond_Pattern {
+    public static void main(String[] args) {
+        int a=5;
+        for (int i=1;i<6;i++){
+            for (int j=a;j>0;j--){
+                System.out.print(" ");
+            }
+            for (int k=i;k>0;k--){
+                System.out.print("*"+" ");
+            }
+            a--;
+            System.out.println();
+        }
+        int b=1;
+        for(int i=4;i>0;i--){
+            for (int j=b;j>=0;j--){
+                System.out.print(" ");
+            }
+            for (int k=i;k>0;k--){
+                System.out.print("*"+" ");
+            }b++;
+            System.out.println();
+        }
+    }
+}
---
+++ src/Rhombus_Pattern.java
@@ -0,0 +1,13 @@
+public class Rhombus_Pattern {
+    public static void main(String[] args) {
+        for (int i=1;i<6;i++){
+            for(int j=1;j<i;j++){
+                System.out.print(" ");
+            }
+            for (int k=1;k<6;k++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
